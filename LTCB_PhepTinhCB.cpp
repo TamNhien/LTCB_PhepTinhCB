53bc9a814de73e5ab772047bbe4a8431782bd60b
TamNhien <canhthien91@gmail.com>
2024-02-26 10:26:34 +0700

    Add project files.

---
+++ LTCB_PhepTinhCB.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int tinhtong(int so1, int so2) {
+	return so1 + so2;
+}
+
+int main() {
+	int so1, so2,tong;
+	cout << "Chuong trinh tinh Thuong 2 so\n";
+	cout << " Nhap so thu nhat : ";
+	cin >> so1;
+	cout << "Nhap so thu hai : ";
+	cin >> so2;
+	tong = tinhtong(so1, so2);
+	cout << so1 << " + " << so2 << " = " << tong << endl;
+	return 0;
+}
