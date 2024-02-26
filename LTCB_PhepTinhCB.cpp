cd7bd6ebcd12240e296265545941d1081d5a73ba
TamNhien <canhthien91@gmail.com>
2024-02-26 11:20:29 +0700

    cap nhat

--- LTCB_PhepTinhCB.cpp
+++ LTCB_PhepTinhCB.cpp
@@ -4,15 +4,35 @@ using namespace std;
 int tinhtong(int so1, int so2) {
 	return so1 + so2;
 }
-
+int tinhhieu(int so1, int so2) {
+	return so1 - so2;
+}
+int tinhtich(int so1, int so2) {
+	return so1 * so2;
+}
+float tinhthuong(float so1, float so2) {
+	return (float)so1 / so2;
+}
 int main() {
-	int so1, so2,tong;
-	cout << "Chuong trinh tinh Thuong 2 so\n";
+	int so1, so2,ketqua;
+	cout << "Chuong trinh tinh Tong 2 so\n";
 	cout << " Nhap so thu nhat : ";
 	cin >> so1;
 	cout << "Nhap so thu hai : ";
 	cin >> so2;
-	tong = tinhtong(so1, so2);
-	cout << so1 << " + " << so2 << " = " << tong << endl;
+	ketqua = tinhtong(so1, so2);
+	cout << so1 << " + " << so2 << " = " << ketqua << endl;
+	ketqua = tinhhieu(so1, so2);
+	cout << so1 << " - " << so2 << " = " << ketqua << endl;
+	ketqua = tinhtich(so1, so2);
+	cout << so1 << " * " << so2 << " = " << ketqua << endl;
+	ketqua = tinhthuong(so1, so2);
+	if (so2 != 0) {
+		ketqua = (float)so1 / so2;
+		cout << so1 << " / " << so2 << " = " << ketqua << endl;
+	}
+	else {
+		cout << "Khong the chia " << so1 << " cho 0\n";
+	}
 	return 0;
 }
